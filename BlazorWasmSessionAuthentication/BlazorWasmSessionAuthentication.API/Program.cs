6b9787ce9503c6fc10fc1ef9fabd385941d904b6
manvominh <vominhman@gmail.com>
2023-10-15 11:56:05 +0700

    update authorize Administrator + User roles

--- BlazorWasmSessionAuthentication/BlazorWasmSessionAuthentication.API/Program.cs
+++ BlazorWasmSessionAuthentication/BlazorWasmSessionAuthentication.API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+                                                  
 builder.Services.AddCors(policy =>
 {
     policy.AddPolicy("CorsPolicy", opt => opt
